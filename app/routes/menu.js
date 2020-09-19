5a78a8e3789803d9c1396285d010679ea513165c
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-19 20:35:55 +0100

    Fetching data from the menu.json file

--- app/routes/menu.js
+++ app/routes/menu.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default class MenuRoute extends Route {
+    model() {
+        // Getting data from the local menu.json file
+       return fetch('/menu/menu.json')
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.error(err);
+        });
+    }
 }
