477b09a8c55bf051371a43aab191936d373cbcd0
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-19 22:58:28 +0100

    Finished order tracker

--- app/components/order-tracker.js
+++ app/components/order-tracker.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-    orderManager: Ember.inject.service('order-manager')
+    orderManager: Ember.inject.service('order-manager'),
+    actions: {
+        remove(day, menuCategory) {
+            this.get('orderManager').removeMenuOption(day, menuCategory);
+        }
+    }
 });
