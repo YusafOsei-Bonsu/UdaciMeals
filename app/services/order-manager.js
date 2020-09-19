b24e33d917d318e9f76fe2854d82c5ea91f68b62
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-19 22:30:12 +0100

    Completing the order manager

--- app/services/order-manager.js
+++ app/services/order-manager.js
@@ -1,4 +1,21 @@
-import Service from '@ember/service';
+import Ember from 'ember';
 
-export default class OrderManagerService extends Service {
-}
+export default Ember.Service.extend({
+    selectedDay: "Monday",
+    menuSelection: {
+        Monday: {},
+        Tuesday: {},
+        Wednesday: {},
+        Thursday: {},
+        Friday: {},
+    },
+    setSelectedDay(day) {
+        this.set('selectedDay', day);
+    },
+    chooseMenuOption(mealCategory, menuItemName) {
+        this.set('menuSelection', this.get('selectedDay') + mealCategory, menuItemName);
+    },
+    removeMenuOption(day, mealCategory) {
+        this.set('menuSelection.' + day + '.' + mealCategory, '');
+    }
+});
