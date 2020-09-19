a4a4166ccae01f16e8778fce2e2d74ada7da71de
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-19 22:58:43 +0100

    Finished menu-item

--- app/components/menu-item.js
+++ app/components/menu-item.js
@@ -2,5 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     classNames: ['menu-item'],
-    tagName: 'li'
+    tagName: 'li',
+    actions: {
+        changeCategory(category) {
+            this.set('mealCategory', category);
+        },
+        chooseItem(menuItemName) {
+            this.get('orderManager').chooseMenuOption(this.get('mealCategory'), menuItemName);
+        }
+    }
 });
