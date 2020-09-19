21ad291be8fc5abf355606af5d5378b9ec99b3ad
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-19 20:46:38 +0100

    Decided to use Axios

--- app/routes/menu.js
+++ app/routes/menu.js
@@ -1,12 +1,11 @@
 import Route from '@ember/routing/route';
+import axios from 'axios';
 
 export default class MenuRoute extends Route {
     model() {
         // Getting data from the local menu.json file
-       return fetch('/menu/menu.json')
-        .then(response => {
-            return response.json();
-        })
+       return axios.get('/menu/menu.json')
+        .then(response => response.data)
         .catch(err => {
             console.error(err);
         });
