b108834328137c49226a10dd23167e691369e80c
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-19 21:03:37 +0100

    Retrieving data from a certain item

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
+import axios from 'axios';
 
 export default class ItemRoute extends Route {
+    model(params) {
+        const itemName = params.item_name;
+        return axios.get(`/menu/${itemName}.json`)
+            .then(response => {
+                return response.data;
+            }).catch(err => {
+                console.error(err);
+            });
+    }
 }
